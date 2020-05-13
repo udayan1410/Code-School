841274a293f877fbbac874e87e55cbdf132e79f7
udayanbirajdar <udayanbirajdar@gmail.com>
2020-05-13 13:46:09 -0700

    Fixed Login Screen
    Changed code for login.
    Added comments
    Added button cilck listener

--- Project/app/src/main/java/com/codeschool/Utils/Constants.java
+++ Project/app/src/main/java/com/codeschool/Utils/Constants.java
@@ -1,5 +1,10 @@
 package com.codeschool.Utils;
 
 public class Constants {
+    //All Tech news
     public static final String TECH_NEWS = "http://newsapi.org/v2/top-headlines?country=us&category=technology&apiKey=42163898b9524d94b97d876f947ce134";
+
+    //Server URL
+    public static final String SERVER_URL="http://10.0.0.217:8000/";
+
 }
--- Project/app/src/main/java/com/codeschool/project/LoginActivity.java
+++ Project/app/src/main/java/com/codeschool/project/LoginActivity.java
@@ -6,48 +6,87 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
+import android.app.Dialog;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.codeschool.Models.LoginModel;
 import com.codeschool.Models.LoginStatusModel;
 import com.codeschool.Network.NetworkCient;
+import com.codeschool.Utils.Constants;
 import com.codeschool.Utils.Misc;
 
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText loginEmail, loginPassword;
+    Button loginButton;
+    Dialog loadingDialog;
+    NetworkCient.ServerCommunicator communicator;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        LoginModel loginModel = new LoginModel("udayanbirajdar@gmail.com","1234");
+        //Initializing all views
+        init();
 
-        NetworkCient.ServerCommunicator communicator = NetworkCient.getClient("http://10.0.0.217:8000/");
-        Call<LoginStatusModel> loginStatusModelCall = communicator.sendLoginData(loginModel);
-
-        loginStatusModelCall.enqueue(new Callback<LoginStatusModel>() {
+        //Setting on click listener on button
+        loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onResponse(Call<LoginStatusModel> call, Response<LoginStatusModel> response) {
-                LoginStatusModel loginStatusModel = response.body();
+            public void onClick(View v) {
+                //Opening a dialog box to show loading
+                loadingDialog.show();
 
-                if(!loginStatusModel.getStatus().equals("Success")){
-                    Misc.showToast(LoginActivity.this,loginStatusModel.getStatus());
-                }
-                else{
-                    Misc.showToast(LoginActivity.this,"Login Success");
-                }
-
-
-            }
-
-            @Override
-            public void onFailure(Call<LoginStatusModel> call, Throwable t) {
-                Log.d("TAG","ERROR "+t.getMessage());
+                //Making a post request to API
+                LoginModel loginModel = new LoginModel(loginEmail.getText().toString(), loginPassword.getText().toString());
+                communicator = NetworkCient.getClient(Constants.SERVER_URL);
+                Call<LoginStatusModel> loginStatusModelCall = communicator.sendLoginData(loginModel);
+                loginStatusModelCall.enqueue(new LoginCallbackHandler());
             }
         });
-
-
     }
+
+
+    public void init(){
+        loginEmail = findViewById(R.id.loginEmail);
+        loginPassword = findViewById(R.id.loginPassword);
+        loginButton = findViewById(R.id.loginButton);
+
+        loadingDialog = Misc.createDialog(LoginActivity.this,R.layout.dialog_progress);
+    }
+
+
+    //Handling the callback of the post request
+    private class LoginCallbackHandler implements Callback<LoginStatusModel>{
+        @Override
+        public void onResponse(Call<LoginStatusModel> call, Response<LoginStatusModel> response) {
+            //Dismiss dialog box
+            loadingDialog.dismiss();
+
+            //Parse the data
+            LoginStatusModel loginStatusModel = response.body();
+
+            //Checking if status is success
+            if (!loginStatusModel.getStatus().equals("Success"))
+                Misc.showToast(LoginActivity.this, loginStatusModel.getStatus());
+
+             else
+                Misc.showToast(LoginActivity.this, "Login Success");
+
+        }
+
+        //Failed to retrieve data
+        @Override
+        public void onFailure(Call<LoginStatusModel> call, Throwable t) {
+            loadingDialog.dismiss();
+            Misc.showToast(LoginActivity.this,"Failed to Get Data. Check Internet");
+        }
+    }
+
 }
