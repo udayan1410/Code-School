b6295e47a8d6afb0daa4a93426d8b37958f78b86
Kewin Shah <kewinshah25@gmail.com>
2020-06-03 19:28:35 -0700

    Splash Screen

---
+++ Project/app/src/main/java/com/codeschool/project/SplashScreenActivity.java
@@ -0,0 +1,40 @@
+package com.codeschool.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Intent i;
+    ImageView logo;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splashscreen);
+        logo = (ImageView) findViewById(R.id.logo);
+
+
+        this.i = new Intent(this, LoginActivity.class);
+        new Thread() {
+            public void run() {
+                try {
+                    sleep(1800L);
+                    SplashScreenActivity.this.startActivity(SplashScreenActivity.this.i);
+                    SplashScreenActivity.this.finish();
+                    return;
+                } catch (InterruptedException localInterruptedException) {
+                    localInterruptedException = localInterruptedException;
+                    SplashScreenActivity.this.startActivity(SplashScreenActivity.this.i);
+                    SplashScreenActivity.this.finish();
+                    return;
+                }
+
+            }
+        }.start();
+    }
+}
